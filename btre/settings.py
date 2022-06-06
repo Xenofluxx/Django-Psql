ca6ede44d6e178088626f6fc95129599dd122ce7
Xenofluxx <93127339+Xenofluxx@users.noreply.github.com>
2022-06-06 15:31:31 +0200

    3
    Prettified

--- btre/settings.py
+++ btre/settings.py
@@ -123,12 +123,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # added static folder to root
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-# added static folder to dir
-STATICFILES_DIR = [os.path.join(BASE_DIR, 'btre/static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'btre/static')
+]
 
 
 # Default primary key field type
